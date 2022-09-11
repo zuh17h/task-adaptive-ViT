87d759b9bbb5539bc6203b7875873a7a33fa7dc7
imj2185 <dancingnum@gmail.com>
2022-09-11 11:59:14 -0400

    dir change

--- train.py
+++ train.py
@@ -305,7 +305,7 @@ def main():
                                                  "ViT-L_32", "ViT-H_14"],
                         default="ViT-B_16",
                         help="Which variant to use.")
-    parser.add_argument("--pretrained_dir", type=str, default="./pretrained",
+    parser.add_argument("--pretrained_dir", type=str, default="./pretrained/ViT-B_16.npz",
                         help="Where to search for pretrained ViT models.")
     parser.add_argument("--pretrained_model", type=str, default=None,
                         help="load pretrained model")
